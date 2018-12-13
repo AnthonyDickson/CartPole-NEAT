8709828d1ad35cb8cbfbbbd0e5bb8e6d10b7e397
Anthony Dickson <eight0153@gmail.com>
2018-12-14 07:15:02 +1300

    start graph refactor

--- graph.py
+++ graph.py
@@ -1,14 +1,42 @@
 from collections import defaultdict
 from enum import Enum
+from math import exp
 import random
+
+import numpy as np
+
+class Activations:
+    @staticmethod
+    def identity(x):
+        return x
+
+    @staticmethod
+    def relu(x):
+        return max(0, x)
+
+    @staticmethod
+    def sigmoid(x):
+        1 / (1 + exp(-x))
+
+    @staticmethod
+    def tanh(x):
+        return (exp(x) - exp(-x)) / (exp(x) + exp(-x))
+
+    @staticmethod
+    def softmax(z):
+        z_exp = np.exp(z)
+        return z_exp / z_exp.sum()
 
 class Node:
     node_count = 0
 
-    def __init__(self):
+    def __init__(self, activation=Activations.identity):
         self.output = 0
         self.prev_output = 0
         self.bias = random.gauss(0, 1)
+        self.activation = activation
+        self.inputs = []
+
         self.id = Node.node_count
         Node.node_count += 1
 
@@ -43,44 +71,46 @@ class Verbosity(Enum):
 
 class Graph:    
     def __init__(self, verbosity=Verbosity.MINIMAL):        
-        self.nodes = [Sensor(), Sensor(), Sensor(), Output(), Hidden()]
+        self.nodes = [Sensor(), Sensor(), Sensor(), Output(), Hidden(activation=Activations.sigmoid)]
         self.sensors = self.nodes[:3]
         self.hidden = [self.nodes[4]]
         self.outputs = [self.nodes[3]]
-        self.connections = defaultdict(lambda: [])
 
         self.verbosity = verbosity
 
         node = self.nodes[3]
 
         for other in [0, 2, 4]:
-            self.connections[node].append(Connection(node, self.nodes[other]))
+            self.add_input(node, other)
 
         node = self.nodes[4]
 
         for other in [0, 1, 3]:
-            self.connections[node].append(Connection(node, self.nodes[other]))
+            self.add_input(node, other)
 
         self.compile()
 
     def compile(self):
         for output in self.outputs:
-            self._mark_recurrent_connections(output, set())
+            self._mark_recurrent_inputs(output, set())
 
-    def _mark_recurrent_connections(self, curr, visited):
+    def _mark_recurrent_inputs(self, curr, visited):
         visited.add(curr)
 
-        for connection in self.connections[curr]:
-            if connection.target in visited:
-                connection.is_recurrent = True
+        for i in curr.inputs:
+            if i.target in visited:
+                i.is_recurrent = True
             else:
-                self._mark_recurrent_connections(connection.target, visited.copy())
+                self._mark_recurrent_inputs(i.target, visited.copy())
 
-    def disable_connection(self, node, other):
-        for connection in self.connections[node]:
-            if connection.target == other:
-                connection.is_enabled = False
-                self.print('Disabling connection between %s and %s.' % (connection.origin, connection.target))
+    def add_input(self, node, other):
+        node.inputs.append(Connection(node, other))
+
+    def disable_input(self, node, other):
+        for i in node.inputs:
+            if i.target == other:
+                i.is_enabled = False
+                self.print('Disabling input from %s to %s.' % (i.origin, i.target))
 
 
     def compute(self, x):
@@ -97,21 +127,21 @@ class Graph:
         for output in self.outputs:
             network_output.append(self._compute_output(output))
 
-        return network_output
+        return Activations.softmax(network_output)
 
     def _compute_output(self, node, level=0):
         self.print('%s%s Computing...' % ('\t' * level, node))
 
         node_output = node.output if isinstance(node, Sensor) else node.bias
 
-        for connection in self.connections[node]:
-            if not connection.is_enabled:
-                self.print('%s%s Connection to this node is disabled!' % ('\t' * (level + 1), connection.target))
-            elif connection.is_recurrent:
-                node_output += connection.weight * connection.target.prev_output
-                self.print('%s%s Output (recurrent): %f' % ('\t' * (level + 1), connection.target, connection.target.prev_output))
+        for i in node.inputs:
+            if not i.is_enabled:
+                self.print('%s%s Input from this node is disabled!' % ('\t' * (level + 1), i.target))
+            elif i.is_recurrent:
+                node_output += i.weight * i.target.prev_output
+                self.print('%s%s Output (recurrent): %f' % ('\t' * (level + 1), i.target, i.target.prev_output))
             else:
-                node_output += connection.weight * self._compute_output(connection.target, level=level + 1)
+                node_output += i.weight * self._compute_output(i.target, level=level + 1)
 
         node.output = node_output
         self.print('%s%s Output: %f' % ('\t' * level, node, node.output))
@@ -120,8 +150,8 @@ class Graph:
 
     def print_connections(self):        
         for node in self.nodes:
-            for connection in self.connections[node]:
-                print(connection)
+            for i in node.inputs:
+                print(i)
 
     def print(self, msg, verbosity=Verbosity.MINIMAL):
         if self.verbosity.value >= verbosity.value:
@@ -137,7 +167,7 @@ if __name__ == '__main__':
     g.compute(x)
     g.compute(x)
 
-    g.disable_connection(g.nodes[4], g.nodes[3])
+    g.disable_input(g.nodes[4], g.nodes[3])
 
     g.compute(x)
     
