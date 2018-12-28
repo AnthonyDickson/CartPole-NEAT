997a376cbc1a0f2232d7a3964573f90da0f00006
Anthony Dickson <eight0153@gmail.com>
2018-12-28 17:55:19 +1300

    Add bootstrap to project and big red button to homepage.

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
+import {Button} from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    state = {
+        msg: null
+    };
+
+    onClick = () => {
+        this.setState({msg: 'You clicked the button, didn\'t you?'});
+    };
+
     render() {
+        const msg = this.state.msg ? this.state.msg : 'Hello, world!';
+
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
-                        Hello, World!
+                        {msg}
                     </p>
+                    <Button color="danger" onClick={this.onClick}>Do not press!</Button>
                     <a
                         className="App-link"
                         href="https://reactjs.org"
