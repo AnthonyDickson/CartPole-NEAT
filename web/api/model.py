e4e187f8139ee9c73d615dd6801eaecb32f2ee2e
Anthony Dickson <eight0153@gmail.com>
2018-12-30 10:10:01 +1300

    Add Node schemas.

--- web/api/model.py
+++ web/api/model.py
@@ -8,6 +8,7 @@ db = SQLAlchemy()
 
 # noinspection PyTypeChecker
 class Run(db.Model):
+    """A training run of NEAT."""
     id = db.Column(db.CHAR(16), primary_key=True)
     start_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
     end_date = db.Column(db.TIMESTAMP)
@@ -20,3 +21,22 @@ class RunSchema(ma.Schema):
     id = fields.String()
     start_date = fields.DateTime()
     end_date = fields.DateTime()
+
+
+class Node(db.Model):
+    """A node in a neural network computational graph."""
+    object_id = db.Column(db.Integer, primary_key=True)
+    run_id = db.Column(db.String(16), db.ForeignKey('run.id'), nullable=False, primary_key=True)
+    id = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(8), nullable=False)
+    activation = db.Column(db.String(8), nullable=False)
+
+
+class NodeSchema(ma.Schema):
+    object_id = fields.Integer()
+    run_id = fields.String()
+    id = fields.Integer()
+    type = fields.String()
+    activation = fields.String()
+
+
